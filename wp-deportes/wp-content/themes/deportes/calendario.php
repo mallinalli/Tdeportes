e34008d76d78710fe97fa493b60bd5d8ed9a77da
jecr <sinnerei@gmail.com>
2014-07-18 14:04:08 -0500

    Avance de Calendario

--- wp-deportes/wp-content/themes/deportes/calendario.php
+++ wp-deportes/wp-content/themes/deportes/calendario.php
@@ -6,29 +6,9 @@ Template Name: calendarioPage
 <?php get_header(); ?>
 <div class="contenido">
 	<h2 class="section-header">Calendario</h2>
-  	<div class="clearfix">
-      
-      <!--Publicidad-->
-      <section class="calendario clearfix">
-        <h3>Liga MX</h2>
-          <div data-cdate="12" class="cal-fecha hlpr-mr">Jornada 1</div>
-          <div data-cdate="13" class="cal-fecha hlpr-mr-t">Jornada 2</div>
-          <div data-cdate="14" class="cal-fecha">Jornada 3</div>
-          <div data-cdate="15" class="cal-fecha hlpr-mr">Jornada 4</div>
-          <div data-cdate="16" class="cal-fecha hlpr-mr-t">Jornada 5</div>
-          <div data-cdate="17" class="cal-fecha">Jornada 6</div>
-          <div data-cdate="18" class="cal-fecha hlpr-mr">Jornada 7</div>
-          <div data-cdate="19" class="cal-fecha hlpr-mr-t">Jornada 8</div>
-          <div data-cdate="20" class="cal-fecha">Jornada 9</div>
-          <div data-cdate="21" class="cal-fecha hlpr-mr">Jornada 10</div>
-          <div data-cdate="22" class="cal-fecha hlpr-mr-t">Jornada 11</div>
-          <div data-cdate="23" class="cal-fecha">Jornada 12</div>
-          <div data-cdate="24" class="cal-fecha hlpr-mr">Jornada 13</div>
-          <div data-cdate="25" class="cal-fecha hlpr-mr-t">Jornada 14</div>
-          <div data-cdate="26" class="cal-fecha">Jornada 15</div>
-          <div data-cdate="27" class="cal-fecha hlpr-mr">Jornada 16</div>
-          <div data-cdate="28" class="cal-fecha hlpr-mr-t">Jornada 17</div>
-      </section>        		
-  	</div>
+  <!--Publicidad-->
+  <section class="calendario">
+    
+  </section>
 </div>
 <?php get_footer(); ?>
