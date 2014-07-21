a2b23a996516a6f164e706242aeb76fa127477aa
jecr <sinnerei@gmail.com>
2014-07-21 16:55:34 -0500

    Banners En Progreso

--- wp-deportes/wp-content/themes/deportes/header.php
+++ wp-deportes/wp-content/themes/deportes/header.php
@@ -33,7 +33,7 @@
       <?php
         /*Ifs para banners*/
 
-        if ( is_home() ) {
+        /*if ( is_home() ) {
           echo '<span style="color:#ECEFEF;">h</span>';
         }
         if ( is_single() ) {
@@ -41,7 +41,7 @@
         }
         if ( is_page() ) {
           echo '<span style="color:#ECEFEF;">s</span>';
-        }
+        }*/
         
       ?>
         <!--[if lt IE 9]>
@@ -70,25 +70,4 @@
         </div>
       </header>
       <div class="contenedor clearfix">
-        <div class="top-banner">
-            <!--/* Publicidad */-->
-            <!--/* Revive Adserver Javascript Tag v3.0.4 */-->
-
-            <script type='text/javascript'><!--//<![CDATA[
-               var m3_u = (location.protocol=='https:'?'https://www.bnrspd.dreamhosters.com/revive/www/delivery/ajs.php':'http://www.bnrspd.dreamhosters.com/revive/www/delivery/ajs.php');
-               var m3_r = Math.floor(Math.random()*99999999999);
-               if (!document.MAX_used) document.MAX_used = ',';
-               document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
-               document.write ("?zoneid=31");
-               document.write ('&amp;cb=' + m3_r);
-               if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
-               document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
-               document.write ("&amp;loc=" + escape(window.location));
-               if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
-               if (document.context) document.write ("&context=" + escape(document.context));
-               if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
-               document.write ("'><\/scr"+"ipt>");
-            //]]>--></script><noscript><a href='http://www.bnrspd.dreamhosters.com/revive/www/delivery/ck.php?n=a44cf116&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://www.bnrspd.dreamhosters.com/revive/www/delivery/avw.php?zoneid=31&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a44cf116' border='0' alt='' /></a></noscript>
-
-          
-        </div>
+        <div class="top-banner"></div>
